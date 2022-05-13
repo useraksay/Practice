b67f97322b5eb6069b70512bbb1a41017cf79ad9
Biradar <akshay.biradar@accenture.com>
2022-05-13 18:52:43 +0530

    LIS

---
+++ src/dp/LongestIncreasingSubSequence.java
@@ -0,0 +1,27 @@
+package dp;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubSequence {
+    public static void main(String[] args) {
+
+    }
+
+    private static int solveLIS(int[] arr){
+        int ans = 0;
+        int n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        dp[0] = 1;
+
+        for (int i = 1; i < n; i++){
+             for (int j = 0; j < i; j++){
+                 if(arr[i] >= arr[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                 }
+             }
+             ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+}
