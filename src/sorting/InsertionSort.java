38ddae694a0be22a9fac2b19ab03afd0e7118ce7
Akshay Biradar <akshay.biradar@sap.com>
2025-07-05 23:03:02 +0530

    InsertionSort

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+package sorting;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 4, 2, 3, 1};
+        insertionSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
